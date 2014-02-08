5228a7437b5f2028e949bcd41f80a21c75129f12
eeue56 <enalicho@gmail.com>
2014-02-08 23:38:59 +0000

    adding support for spotify streams

---
+++ server/streams/spotify-stream.js
@@ -0,0 +1,35 @@
+var Spotify = require('spotify-web');
+
+
+module.exports = function() {
+    var login = function(user, pass, cb){
+        Spotify.login(user, pass, function(err, _spotify) {
+            if(err) throw err;
+            spotify = _spotify;
+            if(cb) {
+                cb();
+            }
+        });
+    }
+
+    var playSong = function(req, res){
+
+        var uri = req.params.uri;
+
+        Spotify.get(uri, function(err, track){
+            track.play()
+            .on('err', function(err){
+                console.error("Failed to play correctly!", err);
+            })
+            .on('finish', function(){
+                console.log("Finshed stream!")
+            })
+            .pipe(req);
+        });
+    }
+
+    return {
+        playSong : playSong,
+        login : login
+    };
+}
