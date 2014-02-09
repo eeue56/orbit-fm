9783421a7499ed25156e754ee0bab08ab0e80349
eeue56 <enalicho@gmail.com>
2014-02-09 00:16:13 +0000

    added app runner

---
+++ server/streams/app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+    http = require('http');
+    path = require('path');
+
+    spotify = require('spotify-stream');
+    soundcloud = require('soundcloud-stream');
+    youtube = require('youtube-stream');
+    
+    app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8040);
+app.set('views', __dirname + '/views');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/soundcloud/:uri', function(req, res) {
+    soundcloud.login();
+    soundcloud.playSong(req, res);
+});
+
+app.get('/spotify/:uri', function(req, res) {
+    spotify.login();
+    spotify.playSong(req, res);
+});
+
+app.get('/youtube/:uri', function(req, res) {
+    youtube.login();
+    youtube.playSong(req, res);
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
