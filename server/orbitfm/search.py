3bd506b4760aab97db9ca147294ac3f89c467c06
eeue56 <enalicho@gmail.com>
2014-02-09 01:03:42 +0000

    adding searching

---
+++ server/orbitfm/search.py
@@ -0,0 +1,51 @@
+import pyen
+
+en = pyen.Pyen("K6O0P8KO54K2GNOFE")
+
+class MagicSong(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__dict__[k] = v 
+
+    def get_first_track(self):
+        return MagicSource(self.tracks[0])
+
+class MagicSource(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__dict__[k] = v 
+
+
+
+def search(words, artist=None, track=None, album=None):
+
+    if all(x is None for x in (artist, track, album)):
+        return []
+
+    params = {
+        "combined" : words,
+        "results" : 10,
+        "limit" : True,
+    }
+
+    if artist is not None:
+        params["artist"] = artist
+
+    if track is not None:
+        params["track"] = track
+
+    if album is not None:
+        params["album"] = album
+
+
+    #TODO: replace playlist static
+    response = pyen.get('playlist/static', **params)
+
+    songs = response['songs']
+
+    tracks = [MagicTrack(song) for song in songs]
+
+
+
+
+
